8e2477fdbf72c2950cbd8776f1f2839940c9a9c3
neEverett <shengyang.cheng@outlook.com>
2017-03-07 23:38:32 +0800

    BUG fix.

--- test.m
+++ test.m
@@ -1,23 +1,23 @@
 function[] = test()
     global quantizationTable;
     quantizationTable = linspace(1,1,8);
-	for i = 2 : 32
+    for i = 2 : 32
         quantizationTable = [quantizationTable, linspace(i,i,8)];
     end
-	
-	load SPM;
-	files = dir('*.jpg');
-	for i = 1 : length(files)
+
+    load SPM;
+    files = dir('*.jpg');
+    for i = 1 : length(files)
         s = files(i).name;
         img = imread(s);
-		[x,y] = size(img(:,:,1));
-		img = changeTo32Level(img, x, y);
-		img = detect(img, SPM, x, y);
-		imwrite(img, [num2str(i),'.jpg']);
-	end
+        [x,y] = size(img(:,:,1));
+        img = changeTo32Level(img, x, y);
+        img = detect(img, SPM, x, y);
+        imwrite(img, [num2str(i),'-result.jpg']);
+    end
 end
-		
-		
+
+
 
 function[img] = changeTo32Level(img, x, y)
     global quantizationTable;
@@ -33,10 +33,10 @@ end
 function[img] = detect(img, SPM, x, y)
     for i = 1:x
         for j = 1:y
-		    if SPM(img(i,j,1),img(i,j,2),img(i,j,3)) >= 0.8  %The threshold. Set it higher to be more exact but may cause more miss
-			    img(i,j,1:3) = 255;
-			else img(i,j,1:3) = 0;
+            if SPM(img(i,j,1),img(i,j,2),img(i,j,3)) >= 0.8  %The threshold. Set it higher to be more exact but may cause more miss
+                img(i,j,1:3) = 255;
+            else img(i,j,1:3) = 0;
             end
-		end
-	end
+        end
+    end
 end
